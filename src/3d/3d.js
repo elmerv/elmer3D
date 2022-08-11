a8e91d74353870843d31c8f2bcd1ac678964fb00
elmerv <elfvasqu@ucsc.edu>
2022-08-11 01:33:39 -0700

    Starting using drei. Added space, fixed plane, and fixed camera. Still need to add texture to model, I have no clue what to do

--- src/3d/3d.js
+++ src/3d/3d.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useRef, useState } from 'react'
-import {OrbitControls } from "@react-three/drei";
+import {OrbitControls, PerspectiveCamera, Stars, Plane, useTexture} from "@react-three/drei";
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import {useFrame, Canvas, useLoader } from '@react-three/fiber'
+import { DoubleSide, TextureLoader } from 'three';
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
@@ -10,7 +11,7 @@ function Box(props) {
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  // useFrame((state, delta) => (ref.current.rotation.x += 0.01))
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
@@ -20,31 +21,58 @@ function Box(props) {
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
+  
       <boxGeometry args={[9, 9, 9]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <meshStandardMaterial side = {DoubleSide} color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   )
 }
 function ModelLoader(props){
-    const obj = useLoader(OBJLoader, 'StandPose.obj')
-    return <primitive {...props} object={obj} />
+    const obj = useLoader(OBJLoader, 'StandPose.obj');
+    const props = useTexture({
+      map: 'PavingStones092_1K_Color.jpg',
+      displacementMap: 'PavingStones092_1K_Displacement.jpg',
+      normalMap: 'PavingStones092_1K_Normal.jpg',
+      roughnessMap: 'PavingStones092_1K_Roughness.jpg',
+      aoMap: 'PavingStones092_1K_AmbientOcclusion.jpg',
+    });
+    return (
+      <mesh>
+        <primitive {...props} object={obj} />
+        <meshStandardMaterial {...props} />
+
+      </mesh>
+        
+    )
+}
+
+function CameraHelper(){
+  const camera = new PerspectiveCamera(60,1,1,3);
+  return <cameraHelper args = {[camera]}/>;
 }
 export default function Scene(){
     return(
     <div style={{ position: "relative", width: "100%", height: 800 }}>
-      <Canvas>
-          <ambientLight intensity={0.5} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <Canvas  style={{ background: "black" }} >
+          <ambientLight intensity={2} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} /> 
           <pointLight position={[-10, -10, -10]} />
-          <OrbitControls />
           {/* {/* <Box position={[-1.2, 0, 0]} /> */}
-          <Box position={[0, 0, 0]}  scale = {6}/>
+          {/* <Box position={[0, 0, 0]}  scale = {6}/> */}
           <ModelLoader rotation = {[0,90,0]} scale = {0.002}></ModelLoader>
-          <mesh position={[0, 0.2, 0]} rotation = {[180,0,0]}>
+          {/* <mesh position={[0, 0, 0]}  rotation={[Math.PI / 2, 0, 0]}>
             <planeBufferGeometry attach="geometry"  args={[5, 5]}/>
-            <meshPhongMaterial attach="material" color="green" />
-          </mesh>
-      </Canvas>
+            <meshStandardMaterial side = {DoubleSide} color="green" />
+          </mesh> */}
+          <Stars />
+          <Plane rotation-x={Math.PI / 2} args={[100, 100, 4, 4]}>
+            <meshBasicMaterial color="blue" wireframe />
+          </Plane>
+          <axesHelper />
+          <OrbitControls />
+          <PerspectiveCamera position={[5, 3, 5]} makeDefault />
+
+    </Canvas>
     </div>
     )
 }
