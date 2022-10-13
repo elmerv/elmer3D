0f3c5b53ec0b5b4be6f0f2be1fff38f069dfba30
Elmer Vasquez <elfvasqu@ucsc.edu>
2022-10-12 21:42:59 -0700

    Updates

---
+++ precache-manifest.0d16bafe1862f473cf094b26d9ab8df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49ce7f6ec84437e805ad",
+    "url": "/static/css/main.8f2cff02.chunk.css"
+  },
+  {
+    "revision": "49ce7f6ec84437e805ad",
+    "url": "/static/js/main.49ce7f6e.chunk.js"
+  },
+  {
+    "revision": "14b060c7915162ae1f5f",
+    "url": "/static/js/1.14b060c7.chunk.js"
+  },
+  {
+    "revision": "0bfa80062a513d26fe26",
+    "url": "/static/js/2.0bfa8006.chunk.js"
+  },
+  {
+    "revision": "c8dbfd972bc17939af0e",
+    "url": "/static/js/runtime~main.c8dbfd97.js"
+  },
+  {
+    "revision": "0c9ff4eb48e0a043877f7255d8980b27",
+    "url": "/index.html"
+  }
+];
